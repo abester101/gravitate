bdb738541b982ef2f3c865d86b2a84a2d427823d
Abe Storey <abester101@gmail.com>
2014-02-17 09:02:33 -0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gravitate::Application.config.secret_key_base = '23df118c0124f300fc581307cfd9b9dd8932eede50b845eb993cb6a760d3a896adbfc72019cf2897b3f0a1bfb015d33f154a690c2d82373f093fa2fce5d7d6a8'
